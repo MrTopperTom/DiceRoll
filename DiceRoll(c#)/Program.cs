8047d38eaab60baae7488a4458faf7306adbf987
Tom Harrison <83875418+MrTopperTom@users.noreply.github.com>
2021-07-23 00:01:20 +0100

    Add files via upload

---
+++ DiceRoll(c#)/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading;
+//made by TopperTom
+namespace TomsProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Display
+            Console.Title = "DICEROLL";
+            Console.ForegroundColor = ConsoleColor.Cyan;
+
+            //Welcome
+            Console.WriteLine("Welcome to Dice Roll\nMade by TopperTom!\nTry roll a 6\n");
+
+            Random numberGen = new Random();
+            int roll = 0;
+            int attemps = 0;
+
+            Console.WriteLine("Press enter to roll the dice.");
+           
+           while (roll != 6)
+           {
+               Console.ReadKey();
+               roll = numberGen.Next(1, 7);
+               Console.WriteLine("You roled a " + roll);
+               attemps++;
+           }
+
+           Console.WriteLine("\nIt took you " + attemps + " attemps to roll a 6");
+
+            //Wait before close
+            Console.ReadKey();
+        }
+    }
+}
